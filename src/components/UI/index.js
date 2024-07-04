719f9be368390fb9c082b0ed72b6ea96fcaf336c
Alexey Melnikov <lexxxamelnikov@gmail.com>
2024-07-04 17:14:26 +0300

    fixed deleting posts bugs

--- src/components/UI/index.js
+++
@@ -1,6 +0,0 @@
-import Postbutton from '@/components/UI/PostButton.vue';
-import PostBlock from '@/components/UI/PostBlock.vue';
-import PostPage from '@/components/UI/PostPage.vue';
-import Navbar from '@/components/UI/Navbar.vue'
-
-export default [Postbutton,PostBlock,PostPage,Navbar];
